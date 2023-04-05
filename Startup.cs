93a5e4567ef8d83a3db4c9ceffec417e24327d18
counter-cowboy <79208562003@ya.ru>
2023-04-05 15:39:07 +0300

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCompany
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {    //добавляем поддержку контроллеров и представлений MVC
+            services.AddControllersWithViews()
+                //выставляем совместимость с asp.net core 3.0
+                .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.
+                CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {//порядок регистрации middleware очень важен
+            //в процессе разработки нам важно видеть подробную инфу об ошибках
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            //подключаем поддержку статичных файлов (css, js и т.д.)
+            app.UseStaticFiles();
+            //регистрируем нужные нам маршруты (эндпоинты)
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
