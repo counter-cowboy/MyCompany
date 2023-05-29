aeba8b21b4912950f79c31fa93bf37120a4f7e57
counter-cowboy <79208562003@ya.ru>
2023-05-29 16:34:10 +0300

    Add project files.

--- Startup.cs
+++ Startup.cs
@@ -1,38 +1,95 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MyCompany.Domain;
+using MyCompany.Domain.Repositories.Abstract;
+using MyCompany.Domain.Repositories.EntityFramework;
+using MyCompany.Service;
 
 namespace MyCompany
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection services)
-        {    //добавляем поддержку контроллеров и представлений MVC
-            services.AddControllersWithViews()
-                //выставляем совместимость с asp.net core 3.0
-                .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.
-                CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
-        }
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {//порядок регистрации middleware очень важен
-            //в процессе разработки нам важно видеть подробную инфу об ошибках
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
 
-            app.UseRouting();
-            //подключаем поддержку статичных файлов (css, js и т.д.)
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //подключаем конфиг из appsetting.json
+            Configuration.Bind("Project", new Config());
+
+            //подключаем нужный функционал приложения в качестве сервисов
+            services.AddTransient<ITextFieldsRepository, EFTextFieldsRepository>();
+            services.AddTransient<IServiceItemsRepository, EFServiceItemsRepository>();
+            services.AddTransient<DataManager>();
+
+            //подключаем контекст БД
+            services.AddDbContext<AppDbContext>(x => x.UseSqlServer(Config.ConnectionString));
+
+            //настраиваем identity систему
+            services.AddIdentity<IdentityUser, IdentityRole>(opts =>
+            {
+                opts.User.RequireUniqueEmail = true;
+                opts.Password.RequiredLength = 6;
+                opts.Password.RequireNonAlphanumeric = false;
+                opts.Password.RequireLowercase = false;
+                opts.Password.RequireUppercase = false;
+                opts.Password.RequireDigit = false;
+            }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+            //настраиваем authentication cookie
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.Name = "myCompanyAuth";
+                options.Cookie.HttpOnly = true;
+                options.LoginPath = "/account/login";
+                options.AccessDeniedPath = "/account/accessdenied";
+                options.SlidingExpiration = true;
+            });
+
+            //настраиваем политику авторизации для Admin area
+            services.AddAuthorization(x =>
+            {
+                x.AddPolicy("AdminArea", policy => { policy.RequireRole("admin"); });
+            });
+
+            //добавляем сервисы для контроллеров и представлений (MVC)
+            services.AddControllersWithViews(x =>
+                {
+                    x.Conventions.Add(new AdminAreaAuthorization("Admin", "AdminArea"));
+                })
+                //выставляем совместимость с asp.net core 3.0
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //!!! порядок регистрации middleware очень важен
+
+            //в процессе разработки нам важно видеть какие именно ошибки
+            if (env.IsDevelopment()) 
+                app.UseDeveloperExceptionPage();
+
+            //подключаем поддержку статичных файлов в приложении (css, js и т.д.)
             app.UseStaticFiles();
-            //регистрируем нужные нам маршруты (эндпоинты)
+
+            //подключаем систему маршрутизации
+            app.UseRouting();
+
+            //подключаем аутентификацию и авторизацию
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            //регистриуруем нужные нам маршруты (ендпоинты)
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute("admin", "{area:exists}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
